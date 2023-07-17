256e0e77bf2298b76b90afec9241ee847d7d072d
Amit <balekundriamit7@gmail.com>
2023-07-17 12:57:53 +0530

    Not present error handled

--- rickmorty/src/Components/Characters/Characters.jsx
+++ rickmorty/src/Components/Characters/Characters.jsx
@@ -19,9 +19,32 @@ const Characters = () =>{
         const response = await fetch(url); 
         const data = await response.json();
         console.log("res",data);
-        setCharacters(data.results)
+        
+        try{
+            setCharacters(data.results)
+        }
+        catch(e)
+        {
+            console.error(e)
+            setCharacters(null)
+        }
+        try{
         setNext(data.info.next)
+        }
+        catch(e)
+        {
+            console.error(e)
+            setNext(null)
+        }
+
+        try{
         setPrev(data.info.prev)
+        }
+        catch(e)
+        {
+            console.error(e)
+            setPrev(null)
+        }
         console.log("Buttons",setNext,setPrev);
     }
 
@@ -37,7 +60,7 @@ const Characters = () =>{
         <div style={{ display: "flex", justifyContent:"center",alignItems: "center", margin:"30px"}}>
         <Input variant = 'standard' placeholder="Enter Search" onChange={(e) => {getCharacters(`${API}?name=${e.target.value}`)} }/>
         </div>
-        <Table characters={characters} />
+        { characters!=null ? <Table characters={characters}/>: <div> </div> }
         <div style={{ flexDirection:"row", display: "flex", justifyContent:"center",alignItems: "center", margin:"30px"}}>
         { prevUrl!=null? <Button onClick={ ()=>{getCharacters(prevUrl)}} style={{ marginRight:"20px" }} variant="outlined" >Previous</Button>: <Button style={{ marginRight:"20px" }} disabled={true} >Previous</Button>}
         { nextUrl!=null? <Button onClick={ ()=> {getCharacters(nextUrl)}} variant="contained">Next</Button>: <Button  disabled={true} >Next</Button> }
